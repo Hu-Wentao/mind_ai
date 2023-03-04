a13cdf1a3d365f87de2f3b573e71db70e4b8d469
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-03-05 02:21:46 +0800

    test

---
+++ test/inter_test.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mind_ai/domain/domain.dart';
+import 'package:mind_ai/inter/inter.dart';
+
+main() {
+  test("content", () {
+    final jsMsg = {
+      "role": "assistant",
+      "content": "Hello there! How may I assist you today?"
+    };
+    final content = MsgGpt35ContentDto.fromJson(jsMsg);
+    print("content $content");
+  });
+  test("dto", () {
+    final json = {
+      "from_id": "",
+      "to_id": "from_id",
+      "content": jsonEncode([
+        {
+          "message": {
+            "role": "assistant",
+            "content": "Hello there! How may I assist you today?"
+          },
+          "finish_reason": "stop",
+          "index": 0
+        }
+      ]),
+      "conversation_id": null,
+      "parent_id": null,
+      "model_id": "gpt-3.5-turbo"
+    };
+
+    final msg = MsgGpt35Rsp.fromJson(json);
+    print("msg $msg");
+  });
+}
