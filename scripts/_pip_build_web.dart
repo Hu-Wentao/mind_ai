8c8c740da72bd2fff10202eca6107aca8c5b40a1
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-03-06 01:46:12 +0800

    feat buildWeb ;peanutBuildWeb

--- scripts/_pip_build_web.dart
+++ scripts/_pip_build_web.dart
@@ -3,20 +3,43 @@ import 'utils.dart';
 
 main() async {
 // dart pub global activate peanut
-  await Process.start('dart', ['pub', 'global', 'activate', 'peanut'])
-      .printProcess();
-// flutter build web --web-renderer auto --release
-  await Process.start('flutter', [
+  // await Process.start('dart', ['pub', 'global', 'activate', 'peanut'])
+  //     .printProcess();
+  // await buildWeb().printProcess();
+
+  await peanutBuildWeb().printProcess();
+}
+
+Future<Process> buildWeb() async {
+  // flutter build web --web-renderer auto --release
+  final args = [
     'build',
     'web',
     '--web-renderer',
     'auto',
     '--release',
     '--dart-define-from-file=../env/prod.json'
-  ]).printProcess();
+  ];
+  print("f.b.w# flutter ${args.join(' ')}");
+  return await Process.start(
+    'flutter',
+    args,
+  );
+}
 
-// dart pub global run peanut -b prod_web
-  await Process.start(
-          'dart', ['pub', 'global', 'run', 'peanut', '-b', 'prod_web'])
-      .printProcess();
+// // dart pub global run peanut -b prod_web
+Future<Process> peanutBuildWeb() async {
+  final args = [
+    'pub',
+    'global',
+    'run',
+    'peanut',
+    '-b',
+    'prod_web',
+  ];
+  print("p.b.w# dart ${args.join(' ')}");
+  return await Process.start(
+    'dart',
+    args,
+  );
 }
