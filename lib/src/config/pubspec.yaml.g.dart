84f9f99a162e647fce9a26a027aca9f514880c87
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-03-06 02:29:29 +0800

    v1.0.2

--- lib/src/config/pubspec.yaml.g.dart
+++ lib/src/config/pubspec.yaml.g.dart
@@ -40,7 +40,7 @@
 // ignore_for_file: prefer_single_quotes
 
 /// Current app version
-const String version = r'1.0.1';
+const String version = r'1.0.2';
 
 /// The major version number: "1" in "1.2.3".
 const int major = 1;
@@ -49,7 +49,7 @@ const int major = 1;
 const int minor = 0;
 
 /// The patch version number: "3" in "1.2.3".
-const int patch = 1;
+const int patch = 2;
 
 /// The pre-release identifier: "foo" in "1.2.3-foo".
 const List<String> pre = <String>[];
@@ -58,7 +58,7 @@ const List<String> pre = <String>[];
 const List<String> build = <String>[];
 
 /// Build date in Unix Time (in seconds)
-const int timestamp = 1678007399;
+const int timestamp = 1678040919;
 
 /// Name [name]
 const String name = r'mind_ai';
