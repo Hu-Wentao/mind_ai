ea67c8ec3fa69d7e4007a603da43a3e5f1b49101
胡文涛 <35894003+Hu-Wentao@users.noreply.github.com>
2023-03-12 12:20:23 +0800

    feat backend.dart OpenAIModule

--- lib/src/config/injector.config.dart
+++ lib/src/config/injector.config.dart
@@ -8,11 +8,11 @@
 import 'package:dio/dio.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:supabase_flutter/supabase_flutter.dart' as _i4;
+import 'package:supabase_flutter/supabase_flutter.dart' as _i6;
 
-import '../application.dart' as _i5;
-import '../application/service.dart' as _i6;
-import '../infra/cloud/backend.dart' as _i7;
+import '../application.dart' as _i4;
+import '../application/service.dart' as _i7;
+import '../infra/cloud/backend.dart' as _i5;
 
 const String _prod = 'prod';
 const String _test = 'test';
@@ -30,11 +30,18 @@ Future<_i1.GetIt> $initGetIt(
     environment,
     environmentFilter,
   );
+  final openAIModule = _$OpenAIModule();
   final service = _$Service();
   final backend = _$Backend();
+  gh.lazySingleton<_i3.Dio>(
+    () => openAIModule.openaiDio(get<_i4.MindAIConfig>()),
+    instanceName: 'OpenAI',
+  );
   gh.lazySingleton<_i3.Dio>(() => service.dio());
-  await gh.lazySingletonAsync<_i4.Supabase>(
-    () => backend.backend(get<_i5.MindAIConfig>()),
+  gh.lazySingleton<_i5.OpenAIInterceptor>(
+      () => openAIModule.openaiInterceptor(get<_i4.MindAIConfig>()));
+  await gh.lazySingletonAsync<_i6.Supabase>(
+    () => backend.backend(get<_i4.MindAIConfig>()),
     registerFor: {
       _prod,
       _test,
@@ -42,14 +49,16 @@ Future<_i1.GetIt> $initGetIt(
     },
     preResolve: true,
   );
-  gh.lazySingleton<_i6.AppService>(() => service.appService(
+  gh.lazySingleton<_i7.AppService>(() => service.appService(
         get<_i3.Dio>(),
-        get<_i4.Supabase>(),
+        get<_i6.Supabase>(),
       ));
-  gh.lazySingleton<_i6.ChatService>(() => service.chatService(get<_i3.Dio>()));
+  gh.lazySingleton<_i7.ChatService>(() => service.chatService(get<_i3.Dio>()));
   return get;
 }
 
-class _$Service extends _i6.Service {}
+class _$OpenAIModule extends _i5.OpenAIModule {}
 
-class _$Backend extends _i7.Backend {}
+class _$Service extends _i7.Service {}
+
+class _$Backend extends _i5.Backend {}
--- lib/src/infra/cloud/backend.dart
+++ lib/src/infra/cloud/backend.dart
@@ -1,3 +1,5 @@
+import 'package:dart_openai/openai.dart';
+import 'package:dio/dio.dart';
 import 'package:get_arch_core/get_arch_core.dart';
 import 'package:mind_ai/src/application.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -13,4 +15,35 @@ abstract class Backend {
         url: cfg.baseUrl,
         anonKey: cfg.backendAnonKey,
       );
+}
+
+@module
+abstract class OpenAIModule {
+  @Named(OpenAIStrings.openai)
+  @lazySingleton
+  Dio openaiDio(MindAIConfig cfg) =>
+      Dio(BaseOptions(baseUrl: '${cfg.baseUrl}/v1/'));
+
+  /// 需要手动set sk
+  @lazySingleton
+  OpenAIInterceptor openaiInterceptor(MindAIConfig cfg) => OpenAIInterceptor();
+}
+
+class OpenAIInterceptor extends Interceptor {
+  String? sk;
+  String? org;
+
+  OpenAIInterceptor({this.sk, this.org});
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    return handler.next(options
+      ..headers.addAll({
+        "Content-Type": (options.method == 'POST' && options.data is FormData)
+            ? "multipart/form-data"
+            : "application/json",
+        if (sk != null) "Authorization": "Bearer $sk",
+        if (org != null) "OpenAI-Organization": '$org',
+      }));
+  }
 }
